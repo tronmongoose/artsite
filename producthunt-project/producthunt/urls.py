f537f768a89e7b65f286a92fb7a62f845a9a13d2
Erik <erik@APPLEs-MacBook-Pro.local>
2019-07-18 07:24:39 -0700

    login updates

--- producthunt-project/producthunt/urls.py
+++ producthunt-project/producthunt/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from art import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home, name='home'),
+    path('', views.home, name='home'),
+    path('accounts/', include('accounts.urls')),
 ]
